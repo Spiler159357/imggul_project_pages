cae22b144a4b1d531ec7888fa00fc16895118c13
GulGyulMyeon <59256213+Spiler159357@users.noreply.github.com>
2026-09-23 22:29:37 +0900

    increased default planner generation count. 10 -> 20

--- public/js/project/planner.js
+++ public/js/project/planner.js
@@ -4,7 +4,7 @@ import { renderSectionHeader } from './manage.js?v=project-visibility-20260830a'
 import { findSituationImage, renderProjectItemCreateModal } from './character.js?v=project-visibility-20260830a';
 import { combinePromptParts, getSituationById } from './situation.js?v=project-visibility-20260830a';
 
-const PLANNER_DEFAULT_IMAGE_COUNT = 10;
+const PLANNER_DEFAULT_IMAGE_COUNT = 20;
 const PLANNER_MIN_IMAGE_COUNT = 1;
 const PLANNER_MAX_IMAGE_COUNT = 100;
 const PLANNER_META_CACHE_TTL_MS = 3000;
